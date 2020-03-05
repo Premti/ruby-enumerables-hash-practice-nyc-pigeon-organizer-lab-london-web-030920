fad981077a4d5caecafb65c6e32ca0ca02ba9e97
premti <premti@gmail.com>
2020-03-05 15:04:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  new_array = []
+    data.each do |key, value|
+      value.each do |color, name| 
+        name.each do |names|
+          if new_hash[names] == names
+            nil 
+          end
+          if new_hash[names] = {}
+            new_hash
+          end
+        end
+      end
+  end
+  new_hash
 end
