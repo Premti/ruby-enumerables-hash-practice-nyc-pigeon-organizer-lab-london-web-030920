8c2ba08ae51e60b015e0c4c0f4811f2486ad12ec
premti <premti@gmail.com>
2020-03-05 15:05:09 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,6 +4,7 @@ def nyc_pigeon_organizer(data)
   new_hash = {}
   new_array = []
     data.each do |key, value|
+      binding.pry
       value.each do |color, name| 
         name.each do |names|
           if new_hash[names] == names
